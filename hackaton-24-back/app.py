f60ef35f48bebf4454288ff50d60d2fb8984308c
Barbier Nathan <n.barbier001@gmail.com>
2024-05-28 12:30:29 +0200

    ajout back-end

---
+++ hackaton-24-back/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+import pandas as pd
+import plotly.express as px
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://freedb_hackaton:cMgX2ygMsNFK#z8@sql.freedb.tech/freedb_hackaton'
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__ = 'road_accident_clean'
+    id = db.Column(db.Integer, primary_key=True)
+    test = db.Column(db.String(500))
+    
+
+# Route Graphe nombre de Médailles par Année par Pays 
+@app.route('/api/test', methods=['GET'])
+def get_data():
+    sql_query = text("SELECT * FROM test")
+    result = db.session.execute(sql_query)
+    return result
+    
+
+
+# RUN APP
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
