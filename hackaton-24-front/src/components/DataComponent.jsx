3e924a9cf4fce8faa1447e32620398fa9674093c
NathanBarbier <81628062+NathanBarbier@users.noreply.github.com>
2024-05-28 17:02:30 +0200

    Créer premier graph (#2)

--- hackaton-24-front/src/components/DataComponent.jsx
+++ hackaton-24-front/src/components/DataComponent.jsx
@@ -2,49 +2,23 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 const DataComponent = () => {
-    // Créer un state pour stocker les données récupérées
     const [data, setData] = useState(null);
 
-    // Utiliser useEffect pour effectuer la requête lorsque le composant est monté
     useEffect(() => {
-        // Fonction asynchrone pour effectuer la requête
-        const fetchData = async () => {
-            try {
-                // Effectuer la requête GET vers l'API
-                const response = await fetch("http://127.0.0.1:8080/api/test");
-
-                console.log(response)
-                // Vérifier si la réponse est OK (200)
-                if (response.ok) {
-                    // Extraire les données JSON de la réponse
-                    const jsonData = await response.json();
-                    // Mettre à jour le state avec les données récupérées
-                    setData(jsonData);
-                } else {
-                    // Gérer les cas où la requête échoue
-                    console.error("Erreur lors de la récupération des données:", response.status);
-                }
-            } catch (error) {
-                // Gérer les erreurs liées à la requête
-                console.error("Erreur lors de la requête:", error);
-            }
-        };
-
-        // Appeler la fonction fetchData pour récupérer les données
-        fetchData();
-
-        // Clean-up function pour éviter les fuites de mémoire
-        return () => {
-            // Optional: Effectuer des actions de nettoyage si nécessaire
-        };
-    }, []); // Passer un tableau vide en dépendance pour que cela soit exécuté une seule fois
+        axios.get('http://127.0.0.1:8080/api/countries')
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error(error.message);
+            });
+    }, []);
 
     return (
         <div>
-            {/* Afficher les données dans le composant */}
             {data ? (
                 <div>
-                    {/* Afficher les données ici */}
+                    <iframe srcDoc={data} style={{width: '100%', height: '500px', border: 'none'}}/>
                 </div>
             ) : (
                 <div>Chargement...</div>
