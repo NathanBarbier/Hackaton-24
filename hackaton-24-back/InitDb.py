cb27dbdfa2a3100a259f9e3bd1031de72b22972b
Pgnt Charly <97946185+Charly750@users.noreply.github.com>
2024-05-29 14:57:39 +0200

    update initdb pour les nouveaux dataset

--- hackaton-24-back/InitDb.py
+++ hackaton-24-back/InitDb.py
@@ -1,30 +1,22 @@
-from pyspark.sql import SparkSession
 import pandas as pd
 from sqlalchemy import create_engine
 
-spark = SparkSession.builder \
-    .appName("ImportDataToMySQL") \
-    .getOrCreate()
-
-db_url = 'mysql+pymysql://freedb_hackaton:cMgX2ygMsNFK#z8@sql.freedb.tech/freedb_hackaton'
+db_url = 'mysql://freedb_hackaton:cMgX2ygMsNFK#z8@sql.freedb.tech/freedb_hackaton'
 
 engine = create_engine(db_url)
 
 csv_files = {
-    'cleaned_athletes.csv': 'athletes',
-    'cleaned_host.csv': 'host',
-    'cleaned_medal.csv': 'medal',
-    'cleaned_result.csv': 'result'
+    'athlete_cleaned.csv': 'athletes',
+    'host_cleaned.csv': 'hosts',
+    'medal_cleaned.csv': 'medals',
+    'result_cleaned.csv': 'results',
+    'Dataset.csv': 'Datasets',
 }
 
 folder_path = 'Clean_Data/'
 
 for file_name, table_name in csv_files.items():
     file_path = folder_path + file_name
-    df_spark = spark.read.option("header", "true").csv(file_path)
-    df_pandas = df_spark.toPandas()
-    
-    df_pandas.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
+    df = pd.read_csv(file_path)
+    df.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
     print(f"Table {table_name} créée avec succès dans la base de données.")
-
-spark.stop()
