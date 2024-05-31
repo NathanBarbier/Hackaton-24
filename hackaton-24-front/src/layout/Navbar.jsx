f983685b51a2a0dee26194f3c18951812d93fcdc
Pgnt Charly <97946185+Charly750@users.noreply.github.com>
2024-05-31 02:59:39 +0200

    update front navbar complètement fonctionnel

--- hackaton-24-front/src/layout/Navbar.jsx
+++ hackaton-24-front/src/layout/Navbar.jsx
@@ -3,117 +3,121 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+  
   return (
     <nav className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-10">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div className="flex flex-shrink-0 items-center">
+            <img className="h-8 w-auto" src="/assets/doubleP24_logo.png" alt="Paris 2024" />
+          </div>
+          <div className="hidden sm:flex sm:space-x-2 flex-1 justify-center">
+            {/* Liens du menu pour les écrans normaux */}
+            <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">
+              Accueil
+            </Link>
+            <Link to="/Prediction" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" >
+              Predictions 2024
+            </Link>
+            <div className="relative">
+              <button className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium focus:outline-none" onClick={toggleDropdown}>
+                Visualisation
+                <svg className="h-5 w-5 inline-block ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                </svg>
+              </button>
+              <div className={`${isDropdownOpen ? 'block' : 'hidden'} absolute bg-white rounded-md mt-2 shadow-lg z-10`}>
+                {/* Contenu du menu de visualisation */}
+                <div className="flex flex-col">
+                  <Link to="/AverageAgeByDiscipline" className="text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium block" style={{ maxWidth: '200px' }} onClick={toggleDropdown}>
+                    Âge moyen par discipline
+                  </Link>
+                  <Link to="/GenderPerformanceByCountry" className="text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium block" style={{ maxWidth: '200px' }} onClick={toggleDropdown}>
+                    Performance par genre et par pays
+                  </Link>
+                  <Link to="/HostPerformance" className="text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium block" style={{ maxWidth: '200px' }} onClick={toggleDropdown}>
+                    Performance des pays hôtes
+                  </Link>
+                  <Link to="/MedalByCoutries" className="text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium block" style={{ maxWidth: '200px' }} onClick={toggleDropdown}>
+                    Médailles par pays
+                  </Link>
+                  <Link to="/MedalByCountriesByYear" className="text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium block" style={{ maxWidth: '200px' }} onClick={toggleDropdown}>
+                    Médailles par pays par année
+                  </Link>
+                  <Link to="/MedalByDisciplineByCountry" className="text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium block" style={{ maxWidth: '200px' }} onClick={toggleDropdown}>
+                    Médailles par discipline par pays
+                  </Link>
+                  <Link to="/Top10Athletes" className="text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium block" style={{ maxWidth: '200px' }} onClick={toggleDropdown}>
+                    Top 10 des athlètes
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          {/* Bouton du menu mobile */}
+          <div className="sm:hidden">
             <button
-              type="button"
-              className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded={isMobileMenuOpen}
               onClick={toggleMobileMenu}
+              className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium focus:outline-none"
             >
-              <span className="sr-only">Open main menu</span>
-              {isMobileMenuOpen ? (
-                <svg
-                  className="block h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  className="block h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-              )}
+              Menu
             </button>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
-            <div className="flex flex-shrink-0 items-center">
-              <img className="h-8 w-auto" src="/assets/doubleP24_logo.png" alt="Paris 2024" />
-            </div>
-            <div className="hidden sm:flex sm:space-x-4">
-              <Link to="/" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">
-                Home
-              </Link>
-              <Link to="/AverageAgeByDiscipline" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                AverageAgeByDiscipline
-              </Link>
-              <Link to="/GenderPerformanceByCountry" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                GenderPerformanceByCountry
-              </Link>
-              <Link to="/HostPerformance" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                HostPerformance
-              </Link>
-              <Link to="/MedalByCoutries" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                MedalByCoutries
-              </Link>
-              <Link to="/MedalByCountriesByYear" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                MedalByCountriesByYear
-              </Link>
-              <Link to="/MedalByDisciplineByCountry" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                MedalByDisciplineByCountry
-              </Link>
-              <Link to="/Top10Athletes" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                Top10Athletes
-              </Link>
+        </div>
+        {/* Menu mobile */}
+        <div className={`${isMobileMenuOpen ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {/* Liens du menu pour les appareils mobiles */}
+            <Link to="/" className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" onClick={closeMobileMenu} aria-current="page">
+              Accueil
+            </Link>
+            <Link to="/Prediction" className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" onClick={closeMobileMenu}>
+              Predictions 2024
+            </Link>
+            <div className="relative">
+              <button className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium focus:outline-none" onClick={toggleDropdown}>
+                Visualisation
+                <svg className="h-5 w-5 inline-block ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                </svg>
+              </button>
+              <div className={`${isDropdownOpen ? 'block' : 'hidden'} absolute bg-white rounded-md mt-2 shadow-lg`}>
+                <Link to="/AverageAgeByDiscipline" className="block text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium" onClick={closeMobileMenu}>
+                  Âge moyen par discipline
+                </Link>
+                <Link to="/GenderPerformanceByCountry" className="block text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium" onClick={closeMobileMenu}>
+                  Performance par genre et par pays
+                </Link>
+                <Link to="/HostPerformance" className="block text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium" onClick={closeMobileMenu}>
+                  Performance des pays hôtes
+                </Link>
+                <Link to="/MedalByCoutries" className="block text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium" onClick={closeMobileMenu}>
+                  Médailles par pays
+                </Link>
+                <Link to="/MedalByCountriesByYear" className="block text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium" onClick={closeMobileMenu}>
+                  Médailles par pays par année
+                </Link>
+                <Link to="/MedalByDisciplineByCountry" className="block text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium" onClick={closeMobileMenu}>
+                  Médailles par discipline par pays
+                </Link>
+                <Link to="/Top10Athletes" className="block text-gray-800 hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium" onClick={closeMobileMenu}>
+                  Top 10 des athlètes
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-
-      <div className={`${isMobileMenuOpen ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          <Link to="/" className="block bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">
-            Home
-          </Link>
-          <Link to="/AverageAgeByDiscipline" className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-            AverageAgeByDiscipline
-          </Link>
-          <Link to="/GenderPerformanceByCountry" className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-            GenderPerformanceByCountry
-          </Link>
-          <Link to="/HostPerformance" className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-            HostPerformance
-          </Link>
-          <Link to="/MedalByCoutries" className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-            MedalByCoutries
-          </Link>
-          <Link to="/MedalByCountriesByYear" className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-            MedalByCountriesByYear
-          </Link>
-          <Link to="/MedalByDisciplineByCountry" className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-            MedalByDisciplineByCountry
-          </Link>
-          <Link to="/Top10Athletes" className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-            Top10Athletes
-          </Link>
         </div>
       </div>
     </nav>
